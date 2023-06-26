6c5709e454a8f31647ea6ac56fba7e47196c3ad9
Nath <ananthapindika739@gmail.com>
2023-06-27 00:04:01 +0700

    Update section services

--- src/Components/Services/SectionServices/SectionServices.jsx
+++ src/Components/Services/SectionServices/SectionServices.jsx
@@ -16,7 +16,7 @@ const SectionServices = () => {
                     </p>
                 </div>  
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {/* Tambahkan komponen atau konten card services di sini */}
+                {/* Buat card komponen*/}
             </div>
             </div>
         </div>
