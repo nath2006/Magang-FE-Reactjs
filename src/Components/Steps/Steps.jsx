39b795b8a3daf2c3161ede85d0eb7110c7438aaf
Nath <ananthapindika739@gmail.com>
2023-07-02 15:38:33 +0700

    update-steps-section

--- src/Components/Steps/Steps.jsx
+++ src/Components/Steps/Steps.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
+import './styles.css';
+import logoDua from '../../asset/img/logo-2.svg';
+
 const Steps = () => {
     return (
         <div>
-            <div className="Steps  bg-[#422e87]">
+            <div className="Steps py-16 bg-[#422e87]">
                 <div className="container">
-
+                    <div className="row">
+                        <h4 className="font-medium text-white text-center">
+                            How to
+                            <span className="text-yellow-400"> Hire </span>
+                            Through
+                            <img src={logoDua} alt="logo-steps-section" width="120"></img>
+                        </h4>
+                    </div>
                 </div>
             </div>
         </div>
