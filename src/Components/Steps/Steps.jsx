d58889e99a6c3d2303cdef53a8af3c5eb86bdb23
Nath <ananthapindika739@gmail.com>
2023-07-04 09:44:04 +0700

    update col

--- src/Components/Steps/Steps.jsx
+++ src/Components/Steps/Steps.jsx
@@ -6,9 +6,8 @@ import RightArrow from '../../asset/img/Arrow_Right.svg';
 
 const Steps = () => {
     return (
-        <div>
             <div className="Steps py-16 bg-[#422e87]">
-                <div className="container">
+                <div className="container mx-auto px-4">
                     <div className="row">
                         <h4 className="font-medium text-white text-center">
                             How to
@@ -16,15 +15,13 @@ const Steps = () => {
                             Through
                             <img src={logoDua} alt="logo-steps-section" width="120"></img>
                         </h4>
-                        <ul className="steps-ul p-3 pt-50 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3">
+                        <ul className="flex space-x-4 py-10 pl-3 mx-auto text-left steps-ul p-3 pt-50 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3">
                             <li className="col">
                                 <span className="step-number">1</span>
                                 <h4 className="text-white">Brief your Business:</h4>
                                 <p>After you fill out the 
-                                        <a className="text-white">
-                                            build your team form,
-                                        </a>
-                                    ,a memeber of our matching team will reach out to you at your selected time-slot; to fully understand your technical needs, culture, and stategic route. 
+                                        <a className="text-white"> build your team form,</a>
+                                     a memeber of our matching team will reach out to you at your selected time-slot; to fully understand your technical needs, culture, and stategic route. 
                                 </p>
                                 <div className="right-arrow"> 
                                     <img src={RightArrow} alt="Right arrow"></img>
@@ -52,12 +49,10 @@ const Steps = () => {
                                     <img src={RightArrow} alt="Right arrow"></img>
                                 </div>
                             </li>
-
                         </ul>
                     </div>
                 </div>
             </div>
-        </div>
     )
 }
 
